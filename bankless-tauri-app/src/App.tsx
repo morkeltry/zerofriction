e4bed05c9fec28f9d1cdf0ef1c462090f04c732c
Piers Powlesland <pierspowlesland@gmail.com>
2025-05-09 19:58:51 +0100

    Add button

--- bankless-tauri-app/src/App.tsx
+++ bankless-tauri-app/src/App.tsx
@@ -8,8 +8,10 @@ import './index.css'
 import { usePrivy } from '@privy-io/react-auth'
 
 import { Spinner } from '@/components/spinner'
+import useSafeWallet from './hooks/useSafeWallet';
 
 function App() {
+  const { safeWallet, sendTx } = useSafeWallet();
   // async function greet() {
   //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   //   setGreetMsg(await invoke("greet", { name }));
@@ -20,6 +22,7 @@ function App() {
   if (!ready) {
     return (
       <div className="flex h-screen w-screen items-center justify-center">
+        <button onClick={sendTx} disabled={!safeWallet}></button>
         <Spinner />
       </div>
     )
